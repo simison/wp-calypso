29c736c67df3da57b3b88451d8cb8ec9c62e763a
ramonjd <ramonjd@gmail.com>
2017-10-26 10:39:56 +1100

    added first ab test for localized addresses

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -108,4 +108,14 @@ export default {
 		defaultVariation: 'hide',
 		allowExistingUsers: true,
 	},
+	domainsCheckoutLocalizedAddresses: {
+		datestamp: '20171025',
+		variations: {
+			showLocalizedAddressFormats: 50,
+			showDefaultAddressFormat: 50,
+		},
+		defaultVariation: 'showDefaultAddressFormat',
+		allowExistingUsers: true,
+		localeTargets: 'any',
+	},
 };
