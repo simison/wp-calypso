031d22e35f84907720a7ec35c9079fd55b6d3065
Mikael Korpela <mikael@ihminen.org>
2017-10-29 20:08:09 +0200

    Add renderCacheKey notes

--- client/hello-world/controller.js
+++ client/hello-world/controller.js
@@ -11,9 +11,14 @@ import React from 'react';
 
 export default {
 	helloDemo( context, next ) {
-		// Shouldn't this change the cache key?
+		// Shouldn't this change the cache key? As explained here:
+		// https://github.com/Automattic/wp-calypso/blob/master/docs/server-side-rendering.md#render-cache
+		// context.cacheQueryKey = 'hello-test';
+		//
+		// Edit: nope, it seems to be `cacheQueryKeys` and it requires also `query`
+		// See: https://github.com/Automattic/wp-calypso/blob/master/server/isomorphic-routing/index.js#L99-L101
+		//
 		// calypso:server-render cache access for key +27s /hello-world
-		// context.renderCacheKey = 'hello-test';
 
 		context.primary = <HelloWorldPrimary />;
 
